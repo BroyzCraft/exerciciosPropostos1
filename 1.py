1bddd9181cac7845b6b6eafaa51e15817d253766
BroyzCraft <contato.brunooliveiramarques@gmail.com>
2022-02-04 13:06:02 -0300

    import

---
+++ 1.py
@@ -0,0 +1,6 @@
+def saudacao(msg='Olá', nome='Usuario'):
+    print(msg, nome)
+
+saudacao()
+saudacao(msg='Bem Vindo')
+saudacao(msg='Bem Vindo', nome='Bruno')
---
+++ 3.py
@@ -0,0 +1,4 @@
+def percent(num, porc):
+    return print(((porc/100) * num) + num)
+
+percent(350, 30)
